af5191123af20625a0218f07fbd64a0f4f8e5c1c
TheDollMaster98 <lorisdalsanto@hotmail.it>
2022-05-12 11:52:54 +0200

    Fix trends

--- index.js
+++ index.js
@@ -51,10 +51,37 @@ app.get(`/api/movie/trends`, (req, resp) => {
       console.log(error);
     });
 });
+/* GIUSTA
+app.get('/api/movie/trends' ,  (req , resp) =>{
+
+
+    console.log("CALLING MOVIE ")
+    const country = req.query.country
+    const axios = require('axios').default;
+    // Make a request for a user with a given ID
+    axios.get("https://api.themoviedb.org/3/trending/movie/week%22,%7B
+        params:{
+            api_key: 'c0af7194607876d6036970e4504abc6d',
+            language: 'it-IT'
+        }
+    }
+    )
+    .then(function (response) {
+        // handle success
+        console.log(response);
+
+        resp.send(response.data)
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+})
+*/
 
 // movies id
 // https://api.themoviedb.org/3/movie/ID?api_key=c0af7194607876d6036970e4504abc6d&language=it-IT
-// https://localhost:2000/movie/
+// https://localhost:2000/movie/:id?id=${id}
 app.get("/api/movie/:id", (req, resp) => {
   const id = req.params.id;
   const axios = require("axios").default;
