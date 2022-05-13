3a4b4cbe0f952de1d637fb7f6a56ba71bb3ecb1a
TheDollMaster98 <lorisdalsanto@hotmail.it>
2022-05-13 12:25:23 +0200

    Ingegnerizzazione

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const APY_KEY = "c0af7194607876d6036970e4504abc6d";
 // API KEY:
 // c0af7194607876d6036970e4504abc6d
 
-// https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US
-// https://api.themoviedb.org/3/genre/movie/list?api_key=c0af7194607876d6036970e4504abc6d&language=en-US
-
 // per giorno:
 // https://api.themoviedb.org/3/trending/movie/day?api_key=c0af7194607876d6036970e4504abc6d&language=en-US
 
@@ -25,14 +22,14 @@ app.get(`/api/movie/trends/:type/:time`, (req, resp) => {
   //const country = req.query.country;
   const type = req.params.type; // all, movie, tv
   const time = req.params.time; // day, week, month, year
-  //const language = req.query.language; // en-US, it-IT, es-ES, ...
+  const language = "it-IT"; // en-US, it-IT, es-ES, ...
 
-  // Make a request for a user with a given ID
+  // Make a request for a user with type (all, movie, tv) and time (day, week, month, year)
   axios
     .get("https://api.themoviedb.org/3/trending/" + type + "/" + time, {
       params: {
         api_key: APY_KEY,
-        language: "it-IT",
+        language: language,
       },
     })
     .then(function (response) {
@@ -52,12 +49,14 @@ app.get(`/api/movie/trends/:type/:time`, (req, resp) => {
 // localhost:2000/movie/3
 app.get("/api/movie/:id", (req, resp) => {
   const id = req.params.id;
+  const language = "it-IT"; // en-US, it-IT, es-ES, ...
+
   // Make a request for a user with a given ID
   axios
     .get("https://api.themoviedb.org/3/movie/" + id, {
       params: {
         api_key: APY_KEY,
-        language: "it-IT",
+        language: language,
       },
     })
     .then(function (response) {
@@ -73,18 +72,19 @@ app.get("/api/movie/:id", (req, resp) => {
 
 // TODO: RICERCA FILM PER TITOLO E TIPO
 // https://api.themoviedb.org/3/search/movie?api_key=c0af7194607876d6036970e4504abc6d&language=it-IT&query=NOME_MOVIE
-// test:
-// localhost:2000/api/movie/search/movie/spiderman
+// localhost:2000/api/title/search/movie/spiderman
 app.get("/api/title/search/:type/:query", (req, resp) => {
-  const type = req.params.type;
-  const query = req.params.query;
+  const type = req.params.type; //movie, tv
+  const query = req.params.query; // spiderman, titanic, ...
+  const language = "it-IT";
   const adult = false;
-  // Make a request for a user with a given ID
+
+  // Make a request for a user with type and query
   axios
     .get("https://api.themoviedb.org/3/search/" + type, {
       params: {
         api_key: APY_KEY,
-        language: "it-IT",
+        language: language,
         query: query,
         include_adult: adult,
       },
@@ -140,18 +140,17 @@ app.get("/api/title/search/:type/:query", (req, resp) => {
 
 // ricerca per genere
 // https://api.themoviedb.org/3/discover/movie?api_key=c0af7194607876d6036970e4504abc6d&language=it-IT&with_genres=ID_GENERE
-// ricerva un max di 20 film per genere (possono avere più generi)
-// TODO: da vedere se la ricerca è random, o se aggiorni cambiano i film (SEMBRA DI NO)
 // localhost:2000/api/movie/search/genere?id_genere=${id}
 app.get("/api/movie/search/:id_genere", (req, resp) => {
   const id_genere = req.params.id_genere;
   const type = req.params.type;
-  // Make a request for a user with a given ID
+  const language = "it-IT";
+  // Make a request for a user with id_genere
   axios
     .get("https://api.themoviedb.org/3/discover/" + type, {
       params: {
         api_key: APY_KEY,
-        language: "it-IT",
+        language: language,
         with_genres: id_genere,
       },
     })
